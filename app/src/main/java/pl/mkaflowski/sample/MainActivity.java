57c55b62c3c50ba896f9a76345b7be8dd284cfe1
mateuszkaflowski <mkaflowski@gmail.com>
2018-10-11 14:42:00 +0200

    first commit

--- app/src/main/java/pl/mkaflowski/sample/MainActivity.java
+++ app/src/main/java/pl/mkaflowski/sample/MainActivity.java
@@ -15,13 +15,14 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         final HouseAdsDialog houseAds = new HouseAdsDialog(MainActivity.this);
-        houseAds.setUrl("http://kofii12345.usermd.net/apps.json"); //URL to Json File
-        houseAds.hideIfAppInstalled(false); //An App's Ad will be shown if it is Installed on the Device.
-//        houseAds.setUsePaletteColor(false);
+        String url = "https://url";
+        houseAds.setUrl(url); //URL to Json File
 //        houseAds.setCardCorners(100); // Set CardView's corner radius.
 //        houseAds.setCtaCorner(100); //Set CTA Button's background radius.
-//        houseAds.setForceLoadFresh(false); //Fetch Json everytime loadAds() is called, true by default, if set to false, Json File's Response is kept untill App is closed!
-//        houseAds.showHeaderIfAvailable(false); //Show Header Image if available, true by default
+        houseAds.hideIfAppInstalled(false); //An App's Ad will be shown if it is Installed on the Device.
+        houseAds.setUsePaletteColor(false);
+        houseAds.showHeaderIfAvailable(false); //Show Header Image if available, true by default
+        houseAds.setForceLoadFresh(false); //Fetch Json everytime loadAds() is called, true by default, if set to false, Json File's Response is kept untill App is closed!
         houseAds.loadAds();
 
         houseAds.setAdListener(new AdListener() {
@@ -37,13 +38,16 @@ public class MainActivity extends AppCompatActivity {
             }
 
             @Override
-            public void onAdClosed() {}
+            public void onAdClosed() {
+            }
 
             @Override
-            public void onAdShown() {}
+            public void onAdShown() {
+            }
 
             @Override
-            public void onAdClicked() {}
+            public void onAdClicked() {
+            }
         });
     }
 }
